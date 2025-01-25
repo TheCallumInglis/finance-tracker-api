5cf19e3e3567487c6a4422c71f0a6d866efe7d0a
Callum Inglis <callum.inglis@4oh4.co.uk>
2025-01-25 22:26:59 +0000

    feat: Add isDefault field to Account model

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,9 +1,21 @@
 import prisma from '../db';
 
 export const getAllAccounts = async () => {
-  return await prisma.account.findMany();
+  return (await prisma.account.findMany()).sort((a, b) => a.name.localeCompare(b.name));
 };
 
-export const addAccount = async (data: any) => {
-  return await prisma.account.create({ data });
+export const addAccount = async (body: any) => {
+  const { name, isDefault, accountTypeId} = body;
+
+  if (isDefault) {
+    // Remove existing default account
+    await prisma.account.updateMany({
+      where: { isDefault: true },
+      data: { isDefault: false },
+    });
+  }
+
+  return await prisma.account.create({
+    data: { name, isDefault: isDefault || false, accountTypeId },
+  });
 };
